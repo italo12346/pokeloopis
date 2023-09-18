0d21ffadf042c8f4d318c5c2ad23a4d4819c0869
lyzzandro <lyzzandro91@gmail.com>
2023-09-18 20:38:08 -0300

    Corrigindo estilos e adicionando tipo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
-import Input from './components/input';
-import Placar from './components/Placar';
-import Pokemon from './components/pokemon';
-import './App.css';
-
+import logo from "./logo.svg";
+import Input from "./components/input";
+import Placar from "./components/Placar";
+import Pokemon from "./components/pokemon";
+import "./App.css";
+import Tipo from "./components/tipo";
 
 function App() {
   return (
     <div className="App">
-      <Placar/>
-      <Pokemon/>
+      <Placar />
+      <Pokemon />
       <Input />
+      <Tipo />
     </div>
   );
 }
