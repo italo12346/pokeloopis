50b866610c99885ad16de354c63d820050409542
Italo souza sampaio <italo12346@gmail.com>
2023-09-18 10:33:26 -0300

    fix
    corrigindo bug

--- src/components/pokemon/index.js
+++ src/components/pokemon/index.js
@@ -1,12 +1,13 @@
-<<<<<<< Updated upstream
-=======
 import "./styles.css";
+import Input from "../input";
 
 const fetchedPokemonList = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0");
 let pokemonList = await fetchedPokemonList.json();
 pokemonList = pokemonList.results;
+
 const randomIndex = Math.round(Math.random() * pokemonList.length);
 const pokemon = pokemonList[randomIndex];
+
 const fetchedPokemon = await fetch(pokemon.url);
 let pokemoninfo = await fetchedPokemon.json();
 let pokemonImg = await pokemoninfo.sprites.front_default
@@ -16,8 +17,10 @@ console.log(pokemoninfo);
 export default function Pokemon () {
   return (
   <div className="pokeCard">
+    {
+      Input.name
+    }
     <img src ={pokemonImg}></img>
   </div>
   );
 }
->>>>>>> Stashed changes
