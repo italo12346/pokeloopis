e835a7ab7dbbcb66a4e05c30e687f5c652ece30f
franks <110301698+FrankieMaciel@users.noreply.github.com>
2023-09-19 21:19:17 -0300

    feat: Comparando strings do componente input

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import Tipo from "./components/tipo";import { useEffect, useState } from 'react'
 function App() {
 
   const [count, setCount] = useState(0);
+  const [record, setRecord] = useState(0);
 
   const [pokemonData,setPokemonData] = useState();
   const [pokemonIndex, setPokemonIndex] = useState();
 
   useEffect(() => {(async () => {
+
     const maxPokemonValue = 1010;
     const minPokemonValue = 1;
     
@@ -20,6 +22,7 @@ function App() {
     
     const fetchedPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomIndex}`);
     let pokemonInfo = await fetchedPokemon.json();
+    console.log(pokemonInfo.name);
     setPokemonData(pokemonInfo);
     setPokemonIndex(randomIndex);
   })()}, [count])
@@ -28,7 +31,7 @@ function App() {
     <div className="App">
       <Placar />
       <Pokemon  pokemonData={pokemonData} pokemonIndex={pokemonIndex}/>
-      <Input setCount={setCount} count={count}/>
+      <Input setCount={setCount} count={count} pokemonData={pokemonData} setRecord={setRecord} record={record}/>
       <Tipo />
     </div>
   );
--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,17 +1,32 @@
+import { useState } from "react";
 import inputStyle from "./style.css";
 
-export default function Input ({setCount, count}) {
+export default function Input ({setCount, count, pokemonData, setRecord, record}) {
+
+  const [inputText, setInputText] = useState('');
+
+  function saveLastChange(e) {
+    setInputText(e.target.value);
+  }
 
   function handleSubmit (event) {
-    if (event.key == 'Enter') {
-      console.log('teste');
+
+    if (event.key !== 'Enter') return;
+    
+    const name = pokemonData.name.toLowerCase();
+    const userInput =inputText.toLowerCase();
+
+    if (name === userInput) {
+      console.log('Acertou!');
+      setCount(count++);
+    } else {
       setCount(count++);
     }
   }
 
   return (
   <div style={inputStyle.div}>
-      <input onKeyPress={handleSubmit} style={inputStyle.input} name="hit" type="text" placeholder="Adivinhe o Pokemon"></input>
+      <input onChange={saveLastChange} onKeyPress={handleSubmit} style={inputStyle.input} name="hit" type="text" placeholder="Adivinhe o Pokemon"></input>
   </div>
   );
 }
