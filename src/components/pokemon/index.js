5867772e30ebc08dc7d6235c0cd1a66f7723511f
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2023-09-20 21:37:28 -0300

    fix: corrige a imagem arrastável

--- src/components/pokemon/index.js
+++ src/components/pokemon/index.js
@@ -1,8 +1,7 @@
 import { useEffect } from "react";
 import "./style.css";
 
-export default function Pokemon ({pokemonData, pokemonIndex}) {
-
+export default function Pokemon ({pokemonType, pokemonIndex}) {
   function fixIndexValue (index) {
     if (index < 10) return '00' + index;
     if (index < 100) return '0' + index;
@@ -11,9 +10,9 @@ export default function Pokemon ({pokemonData, pokemonIndex}) {
 
   return (
 
-    <div className="pokeCard">
-           { <img className="missImg" src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${fixIndexValue(pokemonIndex)}.png`}></img>}
-       </div>
+    <div style={{ backgroundColor: pokemonType?.color }} className="pokeCard">
+      { <img className="missImg" draggable="false" src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${fixIndexValue(pokemonIndex)}.png`}></img>}
+    </div>
   )
 
   // if (checkHit() === true) {
