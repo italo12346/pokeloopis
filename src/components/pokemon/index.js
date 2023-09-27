431616710ce8a4ba0fbef3477a94f64b7cc20296
Italo souza sampaio <italo12346@gmail.com>
2023-09-27 16:23:18 -0300

    fix: corrigindo algumas linhas que esquecemos de apagar

--- src/components/pokemon/index.js
+++ src/components/pokemon/index.js
@@ -7,27 +7,11 @@ export default function Pokemon({ pokemonType, pokemonIndex, showPokemon }) {
     if (index < 100) return '0' + index;
     return index;
   }
-
-  const [showImg, setImg] = useState("missImg");
-
-  const indexRef = useRef(pokemonIndex);
-
-  useEffect(() => {
-    let timeout;
-    if (pokemonIndex !== indexRef.current) {
-      setImg("show");
-      indexRef.current = pokemonIndex;
-    }
-
-    return () => clearTimeout(timeout);
-  }, [pokemonIndex]);
-
   return (
     <div style={{ backgroundColor: pokemonType?.color }} className="pokeCard">
       {pokemonIndex !== undefined && (
         <img
           style={{filter: showPokemon}}
-          className={`${showImg}`}
           draggable="false"
           src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${fixIndexValue(pokemonIndex)}.png`}
         />
